d4e771b43dccf176d04d73af6267b9c9db898639
Oluwatobi Adewole <tobiadewole565@gmail.com>
2026-01-17 21:48:17 +0100

    Menu Update
    Trying to make the menu responsive on mobile.

--- script.js
+++ script.js
@@ -24,13 +24,30 @@
 // });
 
 
-const navLinks = document.querySelector('.nav-links')
-const toggleBtn = document.querySelector('.menu-toggle')
+const menu = document.querySelector(".nav-links");
+const menuItems = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger-container");
+const closeIcon= document.querySelector(".menu-close");
+const menuIcon = document.querySelector(".menu-open");
 
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
 
-toggleBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
+hamburger.addEventListener("click", toggleMenu);
 
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
 
 
